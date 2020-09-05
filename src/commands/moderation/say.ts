fe26bb8a8111214a87082f4fa767691368100646
Alviannn <alvianndq@gmail.com>
2020-09-05 23:03:51 +0700

    Added embed title support in say command

--- src/commands/moderation/say.ts
+++ src/commands/moderation/say.ts
@@ -23,7 +23,7 @@ class SayCommand extends Command {
          * Handles filtering a single parameter
          * this'll try to find the parameter and then removes it from the message content
          * 
-         * @param param the parameter value
+         * @param param the accepted parameter
          * @returns `true` if the parameter is found, otherwise `false`
          */
         function filterSingleParam(param: string): boolean {
@@ -31,15 +31,56 @@ class SayCommand extends Command {
             const result = list.filter(str => str !== param);
 
             content = result.join(' ');
-            return !!list.find(str => str === param);
+            return list.length !== result.length;
+        }
+
+        /**
+         * Handles filtering a double parameter or key value pair
+         * 
+         * The sample is like `-t \hey guys\` where the `-t` is the key
+         * and the `\hey guys\` is the value.
+         * 
+         * The value must starts and ends with '\'
+         * 
+         * @param param the accepted parameter
+         * @returns the key-value pair in object, ex: {key: '-t', value: 'hey guys'}
+         */
+        function filterDoubleParam(param: string) {
+            const list = content.split(' ');
+            // the param index
+            const index = list.findIndex(str => str === param);
+            // if the index isn't found or there's no value, failed
+            if (index < 0 || list.length - 1 <= index)
+                return {};
+
+            const regex = /\\(.+)\\/gi;
+            const joinMessage = list.splice(index).join(' ');
+
+            let value: string;
+
+            const found = regex.exec(joinMessage);
+            if (found) {
+                value = found[1];
+                content = content.replace(param, '').replace(found[0], '');
+            } else {
+                return {};
+            }
+
+            return { key: param, value: value };
         }
 
         let finalMessage: string | MessageEmbed;
-        if (filterSingleParam('-em'))
+        const title = filterDoubleParam('-t');
+
+        if (filterSingleParam('-em')) {
             finalMessage = new MessageEmbed()
                 .setDescription(content);
-        else
+
+            if (title)
+                finalMessage.setTitle(title.value);
+        } else {
             finalMessage = content;
+        }
 
         return channel.send(finalMessage);
     }
@@ -53,9 +94,6 @@ class SayCommand extends Command {
         let result: RegExpExecArray | null;
 
         while ((result = regex.exec(content)) !== null) {
-            if (result.index === regex.lastIndex)
-                regex.lastIndex++;
-
             const fullEmoji = result[0];
             const emojiName = result[1];
 
