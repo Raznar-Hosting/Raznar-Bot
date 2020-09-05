f1a46b555265ab3cda651e60740ea851f77a4e6d
Alviannn <alvianndq@gmail.com>
2020-09-05 21:18:46 +0700

    Say command supports weirdly formatted emoji usage

--- src/commands/moderation/say.ts
+++ src/commands/moderation/say.ts
@@ -15,8 +15,31 @@ class SayCommand extends Command {
         if (!args[0])
             return channel.send(`Usage: ${prefix}say <message>`);
 
-        const content = args.join(' ');
+        let content = args.join(' ');
+        content = this.filterEmoji(content);
+
         return channel.send(content);
+    }
+
+    private filterEmoji(content: string): string {
+        const filter = /{[a-zA-Z0-9_-]+}/gi;
+        let newContent = content;
+        let result: RegExpExecArray | null;
+
+        while ((result = filter.exec(content)) !== null) {
+            if (result.index >= filter.lastIndex)
+                filter.lastIndex++;
+
+            const fullEmoji = result[0];
+            const emoji = result[1];
+
+            if (!fullEmoji || !emoji)
+                continue;
+
+            newContent = content.replace(fullEmoji, `:${emoji}:`);
+        }
+
+        return newContent;
     }
 
 }
